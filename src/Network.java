c1cb1497c02a0bc4ea3e961dd953d8092eab7b2e
xlirate <xlirate@live.ca>
2016-10-20 22:09:11 -0400

    Finished implementing, fixing, and touching up Node and Network

--- src/Network.java
+++ src/Network.java
@@ -1,96 +1,83 @@
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Network {
-	
-	private Map<String,Node> nodeNetwork;
-	
+
+	private Map<String, Node> nodeNetwork;
+
 	public Network() {
 		nodeNetwork = new HashMap<>();
 	}
-	
+
 	public boolean create(String id) {
-		if(nodeNetwork.containsKey(id)) {
+		if (nodeNetwork.containsKey(id)) {
 			return false;
 		}
-		
-		Node newNode = new Node(id);
-		nodeNetwork.put(id, newNode);
+
+		nodeNetwork.put(id, new Node(id));
 		return true;
 	}
-	
+
 	public boolean remove(String id) {
-		
-		if(!nodeNetwork.containsKey(id)) {
+		if (!nodeNetwork.containsKey(id)) {
 			return false;
 		}
-		
-		Set<Node> nodeNeighbours = new HashSet<Node>();
-		nodeNeighbours = nodeNetwork.get(id).getNeighbours();
-		
-		
-		for(Node currentNeighbour : nodeNeighbours) {
-			disconnect(nodeNetwork.get(id).getID(),currentNeighbour.getID()); //disconnect the node from all neighbours (and all neighbours from the node)
+
+		for (String n : nodeNetwork.keySet()) {
+			disconnect(id, n); // disconnect the node from all neighbours (and
+								// all neighbours from the node)
 		}
 		nodeNetwork.remove(id);
 		return true;
 	}
-	
+
 	public boolean connect(String a, String b) {
-		
-		if(!nodeNetwork.containsKey(a) || !nodeNetwork.containsKey(b)) {  //check if both nodes exist in the network
-			return false;
-		}
-		
-		nodeNetwork.get(a).addNeighbour(nodeNetwork.get(b)); //add b to a's neighbours
-		nodeNetwork.get(b).addNeighbour(nodeNetwork.get(a)); //add a to b's neighbours
-		return true;
+		boolean outa = nodeNetwork.get(a).addNeighbour(nodeNetwork.get(b));
+		boolean outb = nodeNetwork.get(b).addNeighbour(nodeNetwork.get(a));
+		return outa || outb;
 	}
-	
+
 	public boolean disconnect(String a, String b) {
-		
-		if(!nodeNetwork.containsKey(a) || !nodeNetwork.containsKey(b)) {  //check if both nodes exist in the network
-			return false;
-		}
-		
-		nodeNetwork.get(a).removeNeighbour(nodeNetwork.get(b)); //remove b from a's neighbours
-		nodeNetwork.get(b).removeNeighbour(nodeNetwork.get(a)); //remove a from b's neighbours
-		return true;
+		boolean outa = nodeNetwork.get(a).removeNeighbour(nodeNetwork.get(b));
+		boolean outb = nodeNetwork.get(b).removeNeighbour(nodeNetwork.get(a));
+		return outa || outb;
 	}
-	
+
 	public int send(String from, String to) {
-		
-		if(nodeNetwork.containsKey(from) && nodeNetwork.containsKey(to)) {
-			return nodeNetwork.get(from).sendTo(nodeNetwork.get(to).getID());
+		try {
+			if (nodeNetwork.containsKey(from) && nodeNetwork.containsKey(to)) {
+				return nodeNetwork.get(from).sendTo(nodeNetwork.get(to).getID());
+			} else {
+				System.out.println("\n\nOne or more of the nodes involved do not exist");
+				return -1;
+			}
+		} catch (StackOverflowError e) {
+			System.out.println("\n\nUnable to get to the target node");
+			return -1;
 		}
-		
-		else return 0;
 	}
-	
+
 	public static void main(String args[]) {
 
 		Network testNetwork = new Network();
-		
+
 		testNetwork.create("a");
 		testNetwork.create("b");
 		testNetwork.create("c");
 		testNetwork.create("d");
 		testNetwork.create("e");
-	
+
 		testNetwork.connect("a", "b");
 		testNetwork.connect("a", "c");
 		testNetwork.connect("c", "d");
 		testNetwork.connect("d", "b");
 		testNetwork.connect("a", "e");
 		testNetwork.connect("b", "e");
-		
+
 		testNetwork.remove("d");
-		
+
 		int number_of_hops = testNetwork.send("a", "e");
-		
+
 		System.out.println("number of hops: " + number_of_hops);
 	}
-	
 }
--- src/Node.java
+++ src/Node.java
@@ -1,71 +1,57 @@
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Node {
 
-	private String id;
-	private Set<Node> neighbours;
-	
+	private final String id;
+	private final Set<Node> neighbours;
+
 	public Node(String nodeID) {
-		
 		id = nodeID;
 		neighbours = new HashSet<Node>();
 	}
-	
+
 	public int sendTo(String targetNode) {
-		
+
 		System.out.println(id);
-		
-		int count=0;
-		int randomNeighbour = (int)(Math.random()*neighbours.size());
-		
-		if(this.id==targetNode) {        //we have reached our target node
+
+		if (this.id.equals(targetNode)) { // we have reached our target node
 			return 0;
 		}
-		
-		for(Node currentNode : neighbours) {
-			if(randomNeighbour == count) {
-				return (currentNode.sendTo(targetNode)+1);	//if we have not reached our target node, forward target ID to a random neighbour
+
+		int randomNeighbour = (int) (Math.random() * neighbours.size());
+
+		for (Node currentNode : neighbours) {
+			if (randomNeighbour-- <= 0) {
+				return (currentNode.sendTo(targetNode) + 1); // if we have not
+																// reached our
+																// target node,
+																// forward
+																// target ID to
+																// a random
+																// neighbour
 			}
-			count++;
 		}
-		
+
 		System.out.println("Failed to locate target or adjacent neighbour at node: " + this.getID());
-		return -1;		
+		return -1;
 	}
-	
+
 	public boolean addNeighbour(Node newNeighbour) {
-		
-		if(neighbours.isEmpty()) {
-			neighbours.add(newNeighbour);
-		}
-		
-		else if(neighbours.contains(newNeighbour)) {
-			return false;
-		}
-		
-		else {neighbours.add(newNeighbour);}
-		return true;
+		return neighbours.add(newNeighbour);
 	}
-	
+
 	public boolean removeNeighbour(Node oldNeighbour) {
-		
-		if(!neighbours.contains(oldNeighbour)) {
-			return false;
-		}
-		
-		else {neighbours.remove(oldNeighbour);}
-		return true;
+		return neighbours.remove(oldNeighbour);
 	}
-	
+
 	public Set<Node> getNeighbours() {
-		return neighbours;
+		return Collections.unmodifiableSet(neighbours);
 	}
-	
+
 	public String getID() {
 		return id;
 	}
-	
-	
 }
