5a9d9901493b5326befd23c8b8bf08cd36ce82ab
derekdorey <derekdorey@CB5107-13.labs.sce.carleton.ca>
2016-10-20 21:09:28 -0400

    Implemented Node and Network classes
    Remove() method in Network is throwing ConcurrentModificationException. 
    Everything else is working as intended as far as I've tested.
    Network contains a main method for testing only.

--- src/Network.java
+++ src/Network.java
@@ -1,28 +1,96 @@
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Network {
 	
 	private Map<String,Node> nodeNetwork;
 	
-	public Network() {}
+	public Network() {
+		nodeNetwork = new HashMap<>();
+	}
 	
 	public boolean create(String id) {
+		if(nodeNetwork.containsKey(id)) {
+			return false;
+		}
+		
+		Node newNode = new Node(id);
+		nodeNetwork.put(id, newNode);
 		return true;
 	}
 	
 	public boolean remove(String id) {
+		
+		if(!nodeNetwork.containsKey(id)) {
+			return false;
+		}
+		
+		Set<Node> nodeNeighbours = new HashSet<Node>();
+		nodeNeighbours = nodeNetwork.get(id).getNeighbours();
+		
+		
+		for(Node currentNeighbour : nodeNeighbours) {
+			disconnect(nodeNetwork.get(id).getID(),currentNeighbour.getID()); //disconnect the node from all neighbours (and all neighbours from the node)
+		}
+		nodeNetwork.remove(id);
 		return true;
 	}
 	
 	public boolean connect(String a, String b) {
+		
+		if(!nodeNetwork.containsKey(a) || !nodeNetwork.containsKey(b)) {  //check if both nodes exist in the network
+			return false;
+		}
+		
+		nodeNetwork.get(a).addNeighbour(nodeNetwork.get(b)); //add b to a's neighbours
+		nodeNetwork.get(b).addNeighbour(nodeNetwork.get(a)); //add a to b's neighbours
 		return true;
 	}
 	
 	public boolean disconnect(String a, String b) {
+		
+		if(!nodeNetwork.containsKey(a) || !nodeNetwork.containsKey(b)) {  //check if both nodes exist in the network
+			return false;
+		}
+		
+		nodeNetwork.get(a).removeNeighbour(nodeNetwork.get(b)); //remove b from a's neighbours
+		nodeNetwork.get(b).removeNeighbour(nodeNetwork.get(a)); //remove a from b's neighbours
 		return true;
 	}
 	
 	public int send(String from, String to) {
-		return 0;
+		
+		if(nodeNetwork.containsKey(from) && nodeNetwork.containsKey(to)) {
+			return nodeNetwork.get(from).sendTo(nodeNetwork.get(to).getID());
+		}
+		
+		else return 0;
 	}
+	
+	public static void main(String args[]) {
+
+		Network testNetwork = new Network();
+		
+		testNetwork.create("a");
+		testNetwork.create("b");
+		testNetwork.create("c");
+		testNetwork.create("d");
+		testNetwork.create("e");
+	
+		testNetwork.connect("a", "b");
+		testNetwork.connect("a", "c");
+		testNetwork.connect("c", "d");
+		testNetwork.connect("d", "b");
+		testNetwork.connect("a", "e");
+		testNetwork.connect("b", "e");
+		
+		testNetwork.remove("d");
+		
+		int number_of_hops = testNetwork.send("a", "e");
+		
+		System.out.println("number of hops: " + number_of_hops);
+	}
+	
 }
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,5 @@
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Node {
@@ -6,17 +7,55 @@ public class Node {
 	private String id;
 	private Set<Node> neighbours;
 	
-	public Node(String nodeID) {}
+	public Node(String nodeID) {
+		
+		id = nodeID;
+		neighbours = new HashSet<Node>();
+	}
 	
 	public int sendTo(String targetNode) {
-		return 0;
+		
+		System.out.println(id);
+		
+		int count=0;
+		int randomNeighbour = (int)(Math.random()*neighbours.size());
+		
+		if(this.id==targetNode) {        //we have reached our target node
+			return 0;
+		}
+		
+		for(Node currentNode : neighbours) {
+			if(randomNeighbour == count) {
+				return (currentNode.sendTo(targetNode)+1);	//if we have not reached our target node, forward target ID to a random neighbour
+			}
+			count++;
+		}
+		
+		System.out.println("Failed to locate target or adjacent neighbour at node: " + this.getID());
+		return -1;		
 	}
 	
 	public boolean addNeighbour(Node newNeighbour) {
+		
+		if(neighbours.isEmpty()) {
+			neighbours.add(newNeighbour);
+		}
+		
+		else if(neighbours.contains(newNeighbour)) {
+			return false;
+		}
+		
+		else {neighbours.add(newNeighbour);}
 		return true;
 	}
 	
 	public boolean removeNeighbour(Node oldNeighbour) {
+		
+		if(!neighbours.contains(oldNeighbour)) {
+			return false;
+		}
+		
+		else {neighbours.remove(oldNeighbour);}
 		return true;
 	}
 	
@@ -27,5 +66,6 @@ public class Node {
 	public String getID() {
 		return id;
 	}
+	
 	
 }
