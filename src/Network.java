d113025c786abf9634ddc5a875baf6d10d6e68f8
xlirate <xlirate@live.ca>
2016-11-06 13:58:50 -0500

    Changed the internal workings of Node to allow for a better view

--- src/Network.java
+++ src/Network.java
@@ -78,7 +78,7 @@ public class Network {
 				createNewRandomMessage();
 			}
 			for (Node n : nodeNetwork.values()) {
-				n.prepairToSend();
+				//n.prepairToSend();
 			}
 			for (Node n : nodeNetwork.values()) {
 				n.send();
