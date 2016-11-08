4510f821e56bfe1d09519c1aa447f72a5d01d9f2
Denis Atikpladza <denis12353@hotmail.com>
2016-11-07 20:14:05 -0500

    Added java Docs

--- src/core/Node.java
+++ src/core/Node.java
@@ -23,6 +23,9 @@ public class Node {
 	
 	private final Network network;
 	
+	/**
+	 * constructor for node object
+	 */
 	public Node(Network network, String nodeID) {
 		id = nodeID;
 		neighbours = new HashSet<>();
@@ -30,6 +33,10 @@ public class Node {
 		this.network = network;
 	}
 
+	/**
+	 * method that receives the message and lets the network edit it's
+	 * results via record method
+	 */
 	public void receive(Message m) {
 		if (m.destination.equals(id)) {
 			network.record(m);
@@ -37,20 +44,27 @@ public class Node {
 			buffer.add(m);
 		}
 	}
-
+	
+	/**
+	 * method that prepares a message to be sent and
+	 * ensure that each message is only forwarded once per step
+	 */
 	public void send() {
 		Message m = buffer.poll(); 
 		if(m != null) {		
 			m.incHops();
 			
-			if(!m.getSent()) { //ensure that each message is only forwarded once per step
+			if(!m.getSent()) { 
 				send(m);
 				m.setSent();
 			}
 		}
 	}
 
-	//sends message to random neighbour, determined by a random number
+	/**
+	 * method that sends a message to a random neighbour, 
+	 * determined by a random number
+	 */
 	private void send(Message m) {
 		int randomNeighbour = (int) (Math.random() * neighbours.size());
 
@@ -63,29 +77,45 @@ public class Node {
 		System.out.println(m + " was not forwarded from node:" + id);
 	}
 
+	/**
+	 * adds a neighbour to a node
+	 */
 	public boolean addNeighbour(Node newNeighbour) {
 		return neighbours.add(newNeighbour);
 	}
 
+	/**
+	 * removes a neighbour from a node
+	 */
 	public boolean removeNeighbour(Node oldNeighbour) {
 		return neighbours.remove(oldNeighbour);
 	}
 	
-
-	//getters and setters
+	/**
+	 * gets the neighbours of the node
+	 */
 	public Set<Node> getNeighbours() {
 		return Collections.unmodifiableSet(neighbours);
 		
 	}
 
+	/**
+	 * gets the string ID of the node
+	 */
 	public String getID() {
 		return id;
 	}
 	
+	/**
+	 * gets the contents of the buffer within the node
+	 */
 	public List<Message> getBufferContents(){
 		return Collections.unmodifiableList(buffer);
 	}
 	
+	/**
+	 * gets the network that the node is in
+	 */
 	public Network getNetwork() {
 		return network;
 	}
