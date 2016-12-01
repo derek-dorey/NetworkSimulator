640a0e48461cd5cf727de166b4bc06a9df80175a
xlirate <xlirate@live.ca>
2016-12-01 18:14:59 -0500

    More work on view

---
+++ src/gui/GraphHandler.java
@@ -0,0 +1,108 @@
+package gui;
+
+import java.awt.Component;
+import java.util.List;
+import java.util.Map;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.layout.Layout;
+import org.graphstream.ui.layout.springbox.implementations.LinLog;
+import org.graphstream.ui.swingViewer.ViewPanel;
+import org.graphstream.ui.view.Viewer;
+
+import core.NetworkListener;
+
+public class GraphHandler implements NetworkListener{
+	public Boolean active = true;
+	
+	private Graph graph;
+	private Viewer viewer;
+	private ViewPanel view;
+	private Layout layout;
+	
+	public GraphHandler() {
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+		
+		graph = new SingleGraph("Network");
+		graph.setStrict(false);
+		graph.setAutoCreate( true );
+		
+		graph.addAttribute("ui.stylesheet","url('file:src/com/asmg/access/gui/Network.css')");
+		viewer = new Viewer(graph,Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+		layout = new LinLog();
+		viewer.enableAutoLayout(layout);
+	    view = viewer.addDefaultView(false);
+
+	}
+	
+	public Component getComponent(){
+		return view;
+	}
+	
+	private String edgeId(String nodeId1, String nodeId2){
+		String str1,str2;
+		if(nodeId1.compareTo(nodeId2)>0){
+			str1=nodeId1;
+			str2=nodeId2;
+		}else{
+			str1=nodeId2;
+			str2=nodeId1;
+		}
+		//http://stackoverflow.com/a/35566397
+		return String.format("%s_%s_%s_%s", str1.length(), str2.length(), str1, str2);
+	}
+	
+	@Override
+	public void createNode(String id) {
+		//adds the node iff it does not already exist
+		if(graph.getNode(id) != null){
+			return;
+		}
+		
+		Node nodeVertex = graph.addNode(id);
+		nodeVertex.addAttribute("ui.class", "Node");
+		nodeVertex.addAttribute("ui.label", id);
+				
+	}
+
+	@Override
+	public void destroyNode(String id) {
+		graph.removeNode(id);
+	}
+	
+	@Override
+	public void connectNodes(String id1, String id2) {
+		graph.addEdge(edgeId(id1,id2), id1, id2);
+	}
+
+	@Override
+	public void disconnectNodes(String id1, String id2) {
+		graph.removeEdge(edgeId(id1,id2));
+	}
+
+	@Override
+	public void updateMessages(Map<String, List<Integer>> status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearAll() {
+		for(Node n : graph.getNodeSet()){
+			graph.removeNode(n);
+		}
+	}
+	
+	public void close() {
+		if(graph != null){
+			graph.clear();
+			graph = null;
+		}
+	}
+	
+	public void finalize(){
+		close();
+	}
+}
--- src/gui/ViewFrame.java
+++ src/gui/ViewFrame.java
@@ -10,6 +10,17 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import core.routing.RoutingAlgorithm;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
 
 public class ViewFrame extends JFrame {
 
@@ -36,67 +47,104 @@ public class ViewFrame extends JFrame {
 	 */
 	public ViewFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 813, 578);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{1, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
-		JPanel panel_2 = new JPanel();
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.insets = new Insets(0, 0, 5, 5);
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 0;
-		contentPane.add(panel_2, gbc_panel_2);
-		
-		JPanel panel_1 = new JPanel();
+		JPanel graphPanel = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.gridwidth = 12;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 1;
+		gbc_panel_1.gridx = 0;
 		gbc_panel_1.gridy = 0;
-		contentPane.add(panel_1, gbc_panel_1);
+		contentPane.add(graphPanel, gbc_panel_1);
+		
+		JButton btnUndo = new JButton("Undo");
+		GridBagConstraints gbc_btnUndo = new GridBagConstraints();
+		gbc_btnUndo.insets = new Insets(0, 0, 5, 5);
+		gbc_btnUndo.gridx = 1;
+		gbc_btnUndo.gridy = 1;
+		contentPane.add(btnUndo, gbc_btnUndo);
+		
+		JButton btnLoad = new JButton("Load");
+		GridBagConstraints gbc_btnLoad = new GridBagConstraints();
+		gbc_btnLoad.insets = new Insets(0, 0, 5, 5);
+		gbc_btnLoad.gridx = 2;
+		gbc_btnLoad.gridy = 1;
+		contentPane.add(btnLoad, gbc_btnLoad);
+		
+		JButton btnSave = new JButton("Save");
+		GridBagConstraints gbc_btnSave = new GridBagConstraints();
+		gbc_btnSave.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSave.gridx = 3;
+		gbc_btnSave.gridy = 1;
+		contentPane.add(btnSave, gbc_btnSave);
+		
+		JButton btnCreate = new JButton("Create");
+		GridBagConstraints gbc_btnCreate = new GridBagConstraints();
+		gbc_btnCreate.insets = new Insets(0, 0, 5, 5);
+		gbc_btnCreate.gridx = 4;
+		gbc_btnCreate.gridy = 1;
+		contentPane.add(btnCreate, gbc_btnCreate);
+		
+		JButton btnDestroy = new JButton("Destroy");
+		GridBagConstraints gbc_btnDestroy = new GridBagConstraints();
+		gbc_btnDestroy.insets = new Insets(0, 0, 5, 5);
+		gbc_btnDestroy.gridx = 5;
+		gbc_btnDestroy.gridy = 1;
+		contentPane.add(btnDestroy, gbc_btnDestroy);
+		
+		JButton btnConnect = new JButton("Connect");
+		GridBagConstraints gbc_btnConnect = new GridBagConstraints();
+		gbc_btnConnect.insets = new Insets(0, 0, 5, 5);
+		gbc_btnConnect.gridx = 6;
+		gbc_btnConnect.gridy = 1;
+		contentPane.add(btnConnect, gbc_btnConnect);
+		
+		JButton btnDisconnect = new JButton("Disconnect");
+		GridBagConstraints gbc_btnDisconnect = new GridBagConstraints();
+		gbc_btnDisconnect.insets = new Insets(0, 0, 5, 5);
+		gbc_btnDisconnect.gridx = 7;
+		gbc_btnDisconnect.gridy = 1;
+		contentPane.add(btnDisconnect, gbc_btnDisconnect);
+		
+		JButton btnRate = new JButton("Rate");
+		GridBagConstraints gbc_btnRate = new GridBagConstraints();
+		gbc_btnRate.insets = new Insets(0, 0, 5, 5);
+		gbc_btnRate.gridx = 8;
+		gbc_btnRate.gridy = 1;
+		contentPane.add(btnRate, gbc_btnRate);
+		
+		JButton btnAlgorithm = new JButton("Algorithm");
+		GridBagConstraints gbc_btnAlgorithm = new GridBagConstraints();
+		gbc_btnAlgorithm.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAlgorithm.gridx = 9;
+		gbc_btnAlgorithm.gridy = 1;
+		contentPane.add(btnAlgorithm, gbc_btnAlgorithm);
+		
+		JButton btnStep = new JButton("Step");
+		GridBagConstraints gbc_btnStep = new GridBagConstraints();
+		gbc_btnStep.insets = new Insets(0, 0, 5, 5);
+		gbc_btnStep.gridx = 10;
+		gbc_btnStep.gridy = 1;
+		contentPane.add(btnStep, gbc_btnStep);
 		
 		JPanel panel = new JPanel();
 		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.gridwidth = 2;
+		gbc_panel.insets = new Insets(0, 0, 0, 5);
 		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 1;
+		gbc_panel.gridx = 1;
+		gbc_panel.gridy = 2;
 		contentPane.add(panel, gbc_panel);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
-		
-		JButton btnNewButton = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton.gridx = 1;
-		gbc_btnNewButton.gridy = 0;
-		panel.add(btnNewButton, gbc_btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_1.gridx = 2;
-		gbc_btnNewButton_1.gridy = 0;
-		panel.add(btnNewButton_1, gbc_btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_2.gridx = 3;
-		gbc_btnNewButton_2.gridy = 0;
-		panel.add(btnNewButton_2, gbc_btnNewButton_2);
+		panel.setLayout(new BorderLayout(0, 0));
 	}
 
 }
